bbe23c4d6a4912c9b15226eaba912da8a365ab64
Brandon Rochon <brochon@bu.edu>
2022-08-15 17:00:15 -0700

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -9,45 +9,39 @@ const router = express.Router();
 
 const jwt = require("jsonwebtoken");
 
-const displayProducts = require("./displayProducts");
 const displayStore = require("./displayStore");
 const displayOrders = require("./displayOrders");
+
+const confirmPurchase = require("./confirmPurchase");
+const completePurchase = require("./completePurchase");
 
 const editOrder = require("./editOrder");
 const deleteOrder = require("./deleteOrder");
 const deleteOrderAfterConfirm = require("./deleteOrderAfterConfirm");
 const saveAfterOrderEdit = require("./saveAfterOrderEdit");
 
-const adminManageOrders = require("./adminManageOrders");
-const adminManageUsers = require("./adminManageUsers");
-
-
-const confirmPurchase = require("./confirmPurchase");
-const completePurchase = require("./completePurchase");
-
-
-const addProduct = require("./addProduct");
-const saveProduct = require("./saveProduct");
 const addUser = require("./addUser");
 const saveUser = require("./saveUser");
 const login = require("./login");
 const logout = require("./logout");
-
 const loginAction = require("./loginAction");
-const editProduct = require("./editProduct");
 
+const adminManageOrders = require("./adminManageOrders");
+const adminManageUsers = require("./adminManageUsers");
 const editUser = require("./editUser");
 const deleteUser = require("./deleteUser");
 const deleteUserAfterConfirm = require("./deleteUserAfterConfirm");
-
 const saveAfterUserEdit = require("./saveAfterUserEdit");
 
-
+const displayProducts = require("./displayProducts");
+const addProduct = require("./addProduct");
+const saveProduct = require("./saveProduct");
+const editProduct = require("./editProduct");
 const saveAfterProductEdit = require("./saveAfterProductEdit");
 const deleteProduct = require("./deleteProduct");
 const deleteProductAfterConfirm = require("./deleteProductAfterConfirm");
 
-
+// Middleware
 const checkToken = (req, res, next) => {
   const token = req.signedCookies.access_token;
   if (token) {
@@ -67,12 +61,27 @@ const customerOnly = (req, res, next) => {
   res.redirect("/");
 };
 
+
+// Default to storefront
 router.get("/", function (req, res, next) {
   res.redirect("/store");
 });
 
+// Unauthenticated routes
 router.get("/store", checkToken, displayStore);
+router.get("/register", addUser);
+router.post("/register", saveUser);
+router.get("/login", checkToken, login);
+router.post("/login", loginAction);
+router.get("/logout", logout);
 
+// Routes for authenticated customers
+router.get("/orders", checkToken, customerOnly, displayOrders);
+router.post("/buy", checkToken, customerOnly, confirmPurchase);
+router.post("/reallybuy", checkToken, customerOnly, completePurchase);
+
+
+// Routes for admins
 router.get("/manageorders", checkToken, employeeOnly, adminManageOrders);
 router.get("/manageorders/edit/:id", checkToken, employeeOnly, editOrder);
 router.post("/manageorders/edit", checkToken, employeeOnly, saveAfterOrderEdit);
@@ -84,29 +93,11 @@ router.post(
   deleteOrderAfterConfirm
 );
 
-
-router.get("/orders", checkToken, customerOnly, displayOrders);
-router.post("/buy", checkToken, customerOnly, confirmPurchase);
-router.post("/reallybuy", checkToken, customerOnly, completePurchase);
-
-
-
 router.get("/products", checkToken, employeeOnly, displayProducts);
-
 router.get("/products/add", checkToken, employeeOnly, addProduct);
 router.post("/products/add", checkToken, employeeOnly, saveProduct);
-
-router.get("/register", addUser);
-router.post("/register", saveUser);
-
-router.get("/login", checkToken, login);
-router.post("/login", loginAction);
-
-router.get("/logout", logout);
-
 router.get("/products/edit/:id", checkToken, employeeOnly, editProduct);
 router.post("/products/edit/", checkToken, employeeOnly, saveAfterProductEdit);
-
 router.get("/products/delete/:id", checkToken, employeeOnly, deleteProduct);
 router.post(
   "/products/delete",
